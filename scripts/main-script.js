f7d8ebfcf260baec9263161d1d560d65cda8529b
petrumamartin <64807853+petrumamartin@users.noreply.github.com>
2020-08-06 19:40:46 -0400

    Create main-script.js

---
+++ scripts/main-script.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function(){
+  jQuery('#main-title')
+    .html('<h1>Vue Demonstration</h1>');
+  jQuery('h1')
+    .css({ 'color': 'lavender' });
+  jQuery('#main-title')
+    .append('<h3>Supported by jQuery</h3>');
+
+
+});
